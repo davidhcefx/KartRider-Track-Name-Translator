db018f491590661f93da1759419162e5a1eebb7d
davidhcefx <davidhu0903ex3@gmail.com>
2022-03-03 01:17:08 +0800

    Bugfix: `app.sendFile` should not be allowed to access arbitrary file

--- index.js
+++ index.js
@@ -3,24 +3,29 @@ const app = express();
 const PORT = process.env.PORT || 80;
 
 function _404(req, res) {
-  res.status(404).end(`File ${req.path} not found!`)
+  res.status(404).end(`File ${req.path} not found!`);
+}
+
+function logRequest(req) {
+  console.log([Date.now(), req.ip, req.originalUrl].join('\t'));
 }
 
 app.get('/', (req, res) => {
   res.sendFile(
     'index.html',
-    {root: __dirname},
+    {root: `${__dirname}/web`},
     (err) => {if (err) _404(req, res)}
   );
+  logRequest(req);
 });
 
 app.get('/:name', (req, res) => {
   res.sendFile(
     req.params.name,
-    {root: __dirname},
+    {root: `${__dirname}/web`},
     (err) => {if (err) _404(req, res)}
   );
-  console.log(req.params.name);
+  logRequest(req);
 });
 
 const server = app.listen(PORT, () => {
